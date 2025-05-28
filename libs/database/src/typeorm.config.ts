cece26dfc58c9cf019b169cbdf471a7749c90bc3
Arnaldo Liro <dinholiro@gmail.com>
2025-05-28 11:53:11 -0300

    feat: Setting up database and .env

---
+++ libs/database/src/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from "@nestjs/config";
+import { DataSourceOptions } from "typeorm";
+
+export const getTypeOrmConfig = (configService: ConfigService): DataSourceOptions => ({
+    type: 'oracle',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    sid: configService.get<string>('DB_SID'),
+    entities: [],
+    synchronize: true
+})
