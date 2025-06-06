e1a8e00a94ec76395da35d68584f89cdb6c18946
Arnaldo Liro <dinholiro@gmail.com>
2025-06-06 11:13:03 -0300

    feat: adding docker database container

--- libs/database/src/typeorm.config.ts
+++ libs/database/src/typeorm.config.ts
@@ -2,13 +2,26 @@ import { User } from "@app/common";
 import { ConfigService } from "@nestjs/config";
 import { DataSourceOptions } from "typeorm";
 
+// Database de Dev
+// export const getTypeOrmConfig = (configService: ConfigService): DataSourceOptions => ({
+//     type: 'oracle',
+//     host: configService.get<string>('DB_HOST'),
+//     port: configService.get<number>('DB_PORT'),
+//     username: configService.get<string>('DB_USERNAME'),
+//     password: configService.get<string>('DB_PASSWORD'),
+//     sid: configService.get<string>('DB_SID'),
+//     entities: [User],
+//     synchronize: false
+// })
+
+// Database de Teste
 export const getTypeOrmConfig = (configService: ConfigService): DataSourceOptions => ({
-    type: 'oracle',
+    type: 'postgres',
     host: configService.get<string>('DB_HOST'),
     port: configService.get<number>('DB_PORT'),
     username: configService.get<string>('DB_USERNAME'),
     password: configService.get<string>('DB_PASSWORD'),
-    sid: configService.get<string>('DB_SID'),
+    database: configService.get<string>('DB_NAME'),
     entities: [User],
     synchronize: false
 })
